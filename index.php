d432bec315b1b2f9c036a18fd6b718c23ea74ea3
Rakzol <carlosgilbertojr98@hotmail.com>
2023-08-31 15:06:03 -0700

    testo index

--- index.php
+++ index.php
@@ -1023,7 +1023,7 @@
             <div class="container">
                 <div class="row">
                     <div class="text-center text-md-start mb-3 mb-md-0">
-                        &copy; <a href="#">marverrefacciones.com</a>, Todos los derechos
+                        &copy; <a href="#">marverrefacciones.XD</a>, Todos los derechos
                         reservados.
                     </div>
                 </div>
