e2e282cfd4a3f8fa01e486a07abfe975ff55d928
Rakzol <carlosgilbertojr98@hotmail.com>
2023-09-12 16:20:34 -0700

    cambios

--- estado_cuenta.php
+++ estado_cuenta.php
@@ -430,21 +430,6 @@
     </table>
   </div>
 
-    <ul class="pagination justify-content-center mt-4">
-        <li class="page-item"><a class="page-link" href="#" onclick="event.preventDefault(); pagina_anterior();"
-                id="anterior">&#60;</a></li>
-        <li class="page-item"><a class="page-link" href="#" onclick="event.preventDefault(); primera_pagina();"
-                id="primera">0</a></li>
-        <li class="page-item"><input type="number" class="page-link" id="pagina" placeholder="0" style="width: 5rem;">
-        </li>
-        <li class="page-item"></li><button type="button" class="btn btn-primary" style="border-radius: 0px;"
-            onclick="ir_pagina();">Ir</button></li>
-        <li class="page-item"><a class="page-link" href="#" onclick="event.preventDefault(); ultima_pagina();"
-                id="ultima">0</a></li>
-        <li class="page-item"><a class="page-link" href="#" onclick="event.preventDefault(); siguiente_pagina();"
-                id="siguiente">&#62;</a></li>
-    </ul>
-
     <script>
         let primera;
         let ultima;
