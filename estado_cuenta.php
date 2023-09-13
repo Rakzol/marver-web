349ba6a108903df8b92c611873e17b983cf2c22d
Rakzol <carlosgilbertojr98@hotmail.com>
2023-09-13 14:03:08 -0700

    cambios

--- estado_cuenta.php
+++ estado_cuenta.php
@@ -484,7 +484,7 @@
     $options->set('chroot', __DIR__);
     // $options->set('tempDir', 'tamporaldir');
     $options->set('isHtml5ParserEnabled', true);
-    $options->set('isPhpEnabled', true);
+    // $options->set('isPhpEnabled', true);
     // $options->set('debugPng', true);
     // $options->set('debugCss', true);
 
@@ -497,5 +497,5 @@
     // $debugLog = $options->get('debugLogOutput');
     // file_put_contents('dompdf_debug.log', $debugLog);
 
-    $dompdf->stream("estado_cuenta.pdf", array("Attachment" => false));
+    $dompdf->stream("estado_cuenta.pdf", array("Attachment" => true));
 ?>
