bff1fac77b3bed04f911767613f92f63b92afb39
Rakzol <carlosgilbertojr98@hotmail.com>
2023-09-13 14:02:31 -0700

    cambios

--- estado_cuenta.php
+++ estado_cuenta.php
@@ -480,7 +480,7 @@
     use Dompdf\Options;
 
     $options = new Options();
-    $options->set('isRemoteEnabled', true);
+    // $options->set('isRemoteEnabled', true);
     $options->set('chroot', __DIR__);
     // $options->set('tempDir', 'tamporaldir');
     $options->set('isHtml5ParserEnabled', true);
