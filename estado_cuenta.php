821f1c75b7412ba9048ee6368ba3c9eba1a4b949
Rakzol <carlosgilbertojr98@hotmail.com>
2023-09-13 13:37:09 -0700

    cambios

--- estado_cuenta.php
+++ estado_cuenta.php
@@ -405,7 +405,7 @@
 <body>
 
     <div class="d-flex justify-content-between container mt-3 mb-3 p-3 bg-white rounded" >
-        <img src="https://tauromaquia.jcyl.es/futuretense_cs/JCYL_17/img/Cabecera-150-Tauromaquia.jpg" width="150">
+        <img src="https://www.marverrefacciones.mx/img/logo.png" width="150">
         <div class="text-center" >
             <h5>MARIO ALBERTO VERDUZCO COTA</h5>
             <h5>VECM880923NI1</h5>
