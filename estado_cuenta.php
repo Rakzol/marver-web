ce149aa78acdcdb7382511ae717486136f9228d1
Rakzol <carlosgilbertojr98@hotmail.com>
2023-09-13 13:11:26 -0700

    cambios

--- estado_cuenta.php
+++ estado_cuenta.php
@@ -506,30 +506,30 @@
 
 </html>
 <?php
-    //$html = ob_get_clean();
+    // //$html = ob_get_clean();
 
-    require_once 'dompdf/autoload.inc.php';
+    // require_once 'dompdf/autoload.inc.php';
 
-    use Dompdf\Dompdf;
-    use Dompdf\Options;
+    // use Dompdf\Dompdf;
+    // use Dompdf\Options;
 
-    $options = new Options();
-    $options->set('isRemoteEnabled', true);
-    $options->set('chroot', __DIR__);
-    $options->set('tempDir', 'tamporaldir');
-    $options->set('isHtml5ParserEnabled', true);
-    $options->set('isPhpEnabled', true);
-    $options->set('debugPng', true);
-    //$options->set('debugCss', true);
+    // $options = new Options();
+    // $options->set('isRemoteEnabled', true);
+    // $options->set('chroot', __DIR__);
+    // $options->set('tempDir', 'tamporaldir');
+    // $options->set('isHtml5ParserEnabled', true);
+    // $options->set('isPhpEnabled', true);
+    // $options->set('debugPng', true);
+    // //$options->set('debugCss', true);
 
-    $dompdf = new Dompdf($options);
+    // $dompdf = new Dompdf($options);
 
-    $dompdf->loadHtml(file_get_contents('estado.php'));
+    // $dompdf->loadHtml(file_get_contents('estado.php'));
 
-    $dompdf->render();
+    // $dompdf->render();
 
-    // $debugLog = $options->get('debugLogOutput');
-    // file_put_contents('dompdf_debug.log', $debugLog);
+    // // $debugLog = $options->get('debugLogOutput');
+    // // file_put_contents('dompdf_debug.log', $debugLog);
 
-    $dompdf->stream("estado_cuenta.pdf", array("Attachment" => false));
+    // $dompdf->stream("estado_cuenta.pdf", array("Attachment" => false));
 ?>
