cf9cf12a40009cfa39a5f58ede3de9c741a7828a
Rakzol <carlosgilbertojr98@hotmail.com>
2023-09-14 11:39:52 -0700

    cambios

---
+++ estado_cuenta2.php
@@ -0,0 +1,609 @@
+<?php
+    ob_start();
+    require_once('modelo/inicializar_datos.php');
+    header("Content-Type: text/html");
+?>
+<!DOCTYPE html>
+<html lang='es'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Catalogo</title>
+
+
+
+    <style>
+
+        .card-subtitle::before {
+            content: "$ ";
+        }
+
+        .card-subtitle {
+            color: green;
+        }
+
+        .alert {
+            z-index: 2000;
+            position: fixed !important;
+            right: 1rem;
+            top: 1rem;
+        }
+
+        .imagen-producto {
+            height: 10rem;
+            object-fit: contain;
+        }
+
+        .mx-6 {
+            margin: 1rem !important;
+            margin-left: 4.5rem !important;
+            margin-right: 4.5rem !important;
+        }
+    </style>
+
+    <!-- Favicon -->
+    <link href="img/logo_solo.png" rel="icon">
+
+    <link href="css/estado_fuentes.css" rel="stylesheet">
+
+    <style>
+        :root {
+            --el-color: #E72F2D;
+            --velocidad-iconos: 0.2s;
+        }
+
+        .iconos>i {
+            color: var(--el-color) !important;
+            transition: var(--velocidad-iconos);
+        }
+
+        .iconos {
+            font-size: 1.75rem;
+            transition: var(--velocidad-iconos);
+            background: linear-gradient(0deg, rgba(231, 47, 45, 1) 100%, rgba(231, 47, 45, 1) 100%);
+            background-repeat: no-repeat;
+            background-position: 0rem -4rem;
+        }
+
+        .fondo-instagram {
+            background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%);
+            background-repeat: no-repeat;
+            background-position: 0rem -4rem;
+        }
+
+        .fondo-facebook {
+            background: linear-gradient(0deg, rgba(66, 103, 178, 1) 100%, rgba(66, 103, 178, 1) 100%);
+            background-repeat: no-repeat;
+            background-position: 0rem -4rem;
+        }
+
+        .fondo-whatsapp {
+            background: linear-gradient(0deg, rgba(37, 211, 102, 1) 100%, rgba(37, 211, 102, 1) 100%);
+            background-repeat: no-repeat;
+            background-position: 0rem -4rem;
+        }
+
+        .fondo-messenger {
+            background: linear-gradient(45deg, rgba(9, 147, 255, 1) 25%, rgba(167, 53, 247, 1) 50%, rgba(255, 109, 100, 1) 75%);
+            background-repeat: no-repeat;
+            background-position: 0rem -4rem;
+        }
+
+        .iconos:hover {
+            --el-color: #FFFFFF;
+            background-position: 0rem 0rem;
+        }
+
+        .numero_positivo::before {
+            content: "+";
+        }
+
+        .imagen-alta {
+            width: 100%;
+            top: 2rem;
+            left: 2rem;
+            position: relative;
+            border-radius: 6px;
+        }
+
+        .contenedor-imagen-alta {
+            border: 6px solid #E72F2D;
+            border-radius: 6px;
+            width: calc(100% - 3rem);
+        }
+
+        #carousel-inicio {
+            background: linear-gradient(90deg, #d5201e 0%, rgba(231, 47, 45, 1) 100%);
+            background-size: 15rem auto;
+        }
+
+        #inicio-tips::before,
+        #nosotros::before,
+        #refacciones::before,
+        #proveedores::before,
+        #nosotros_mas::before,
+        #sucursales::before,
+        #contenedor_correo::before {
+            content: '';
+            display: block;
+            height: 0px;
+            margin-top: 25px;
+            visibility: hidden;
+        }
+
+        #sucursales::before {
+            content: '';
+            display: block;
+            height: 70px;
+            margin-top: -70px;
+            visibility: hidden;
+        }
+
+        .mapa {
+            height: 480px;
+            width: 100%;
+            max-width: 900px;
+            display: block;
+            margin: auto;
+        }
+
+        .col-2 {
+            width: 20% !important;
+        }
+
+        #contenedor_redes {
+            margin: auto;
+            margin-top: 1rem;
+        }
+
+        #contenedor_videos {
+            max-width: 100% !important;
+        }
+
+        #nosotros_mas {
+            max-width: 1800px;
+        }
+
+        video,
+        .imagen-video {
+            border-radius: 6px;
+            max-height: 30rem;
+        }
+
+        #contenedor_correo {
+            background: url("img/mail.jpg");
+            background-repeat: no-repeat;
+            background-size: cover;
+            width: 100% !important;
+            max-width: 100% !important;
+            height: 100% !important;
+            max-width: 100% !important;
+            background-position: center;
+            background-attachment: fixed;
+        }
+
+        #contenedor_correo_hijo {
+            background: linear-gradient(180deg, rgb(255 255 255 / 70%) 0%, rgba(0, 0, 0, 0) 100%);
+            border-radius: 12px;
+            padding: 1rem;
+            max-width: 50rem;
+        }
+
+        #contenedor_correo_hijo_parrafo {
+            color: #272727;
+        }
+
+        #contenedor_refacciones {
+            background: linear-gradient(180deg, rgb(255 255 255 / 60%) 0%, rgba(0, 0, 0, 0) 100%);
+            border-radius: 12px;
+            padding: 1rem;
+        }
+
+        .texto-subtitular {
+            position: relative;
+            display: inline-block;
+            text-transform: uppercase;
+        }
+
+        #refacciones {
+            background: url("img/fondo-estatico.jpg");
+            background-repeat: no-repeat;
+            background-size: cover;
+            width: 100% !important;
+            max-width: 100% !important;
+            height: 100% !important;
+            max-width: 100% !important;
+            background-position: center;
+            background-attachment: fixed;
+        }
+
+        .service-item {
+            background: white;
+        }
+
+        @media (max-width: 991px) {
+            .logo_principal {
+                width: 125px !important;
+            }
+        }
+
+        @media (max-width: 768px) {
+            .carrusel_grande {
+                display: none !important;
+            }
+
+            .carrusel_chico {
+                display: block !important;
+            }
+        }
+
+        @media (min-width: 769px) {
+            .carrusel_grande {
+                display: block !important;
+            }
+
+            .carrusel_chico {
+                display: none !important;
+            }
+        }
+
+        @media (min-width: 399px) {
+            #carousel-inicio h5 {
+                font-size: 1.5rem;
+            }
+
+            #carousel-inicio p {
+                font-size: 1.25rem;
+            }
+        }
+
+        @media (min-width: 400px) {
+            #carousel-inicio h5 {
+                font-size: 1.75rem;
+            }
+
+            #carousel-inicio p {
+                font-size: 1.5rem;
+            }
+        }
+
+        @media (min-width: 600px) {
+            #carousel-inicio h5 {
+                font-size: 2.5rem;
+            }
+
+            #carousel-inicio p {
+                font-size: 2.25rem;
+            }
+        }
+
+        @media (min-width: 1200px) {
+            #carousel-inicio h5 {
+                font-size: 3rem;
+            }
+
+            #carousel-inicio p {
+                font-size: 2.75rem;
+            }
+        }
+
+        @media (min-width: 1400px) {
+            #carousel-inicio h5 {
+                font-size: 3.5rem;
+            }
+
+            #carousel-inicio p {
+                font-size: 3.25rem;
+            }
+        }
+
+        #carousel-inicio h5,
+        #carousel-inicio p {
+            margin-bottom: 0;
+            margin-top: 0;
+            /* line-height: 0; */
+            font-weight: 700;
+        }
+
+        #carousel-inicio .carousel-caption {
+            /* border-radius: 6px;
+            background: linear-gradient(180deg, #d5211e6e 0%, rgba(0,0,0,0) 100%); */
+        }
+
+        #contenedor_pie {
+            background: #E72F2D !important;
+            padding-right: 0 !important;
+            padding-left: 0 !important;
+        }
+
+        #contenedor_pie img {
+            height: auto;
+            width: 150px;
+            margin: auto;
+            display: block;
+        }
+
+        #contenedor_pie p,
+        #contenedor_pie a,
+        #contenedor_pie a::before {
+            color: black;
+        }
+
+        .copyright {
+            background: rgb(145 145 145);
+            color: black;
+        }
+
+        .copyright a {
+            color: white !important;
+            text-decoration: none;
+        }
+
+        .metodo_pago {
+            color: white !important;
+            font-size: 4rem;
+            margin-right: 1rem;
+            margin-bottom: 1rem;
+        }
+    </style>
+
+<style>
+        .dinero {
+            color: green;
+        }
+
+        .dinero::before {
+            content: '$';
+        }
+
+        img{
+            vertical-align: middle;
+        }
+
+        .text-center {
+            text-align: center !important;
+        }
+
+        h3, .h3 {
+            font-size: calc(1.3rem + .6vw);
+        }
+
+        h5, .h5 {
+            font-size: 1.25rem;
+        }
+
+        h1, .h1 {
+            font-size: calc(1.375rem + 1.5vw);
+        }
+        
+        h1, .h1, h2, .h2, h3, .h3, h4, .h4, h5, .h5, h6, .h6 {
+            margin-top: 0;
+            margin-bottom: 0.5rem;
+            font-weight: 600;
+            line-height: 1.2;
+            color: #111;
+        }
+
+        .p-3 {
+            padding: 1rem !important;
+        }
+
+        .justify-content-center {
+            justify-content: center !important;
+        }
+        .d-flex {
+            display: flex !important;
+        }
+
+        .mt-2 {
+            margin-top: 0.5rem !important;
+        }
+        .table {
+            --bs-table-bg: rgba(0,0,0,0);
+            --bs-table-striped-color: #777;
+            --bs-table-striped-bg: rgba(0,0,0,0.05);
+            --bs-table-active-color: #777;
+            --bs-table-active-bg: rgba(0,0,0,0.1);
+            --bs-table-hover-color: #777;
+            --bs-table-hover-bg: rgba(0,0,0,0.075);
+            width: 100%;
+            margin-bottom: 1rem;
+            color: #777;
+            vertical-align: top;
+            border-color: #dee2e6;
+        }
+        table {
+            caption-side: bottom;
+            border-collapse: collapse;
+        }
+
+        .table>thead {
+            vertical-align: bottom;
+        }
+        thead, tbody, tfoot, tr, td, th {
+            border-color: inherit;
+            border-style: solid;
+            border-width: 0;
+        }
+
+        .table>:not(:last-child)>:last-child>* {
+            border-bottom-color: currentColor;
+        }
+
+        .table>:not(caption)>*>* {
+            padding: 0.5rem 0.5rem;
+            background-color: var(--bs-table-bg);
+            border-bottom-width: 1px;
+            box-shadow: inset 0 0 0 9999px var(--bs-table-accent-bg);
+        }
+
+th {
+    text-align: inherit;
+    text-align: -webkit-match-parent;
+}
+
+.table>tbody {
+    vertical-align: inherit;
+}
+
+.justify-content-between {
+    justify-content: space-between !important;
+}
+
+</style>
+
+<style>
+    p, h5, h3{
+        margin: 0;
+    }
+
+    body{
+        margin: 0;
+        font-family: "Open Sans",sans-serif;
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.5;
+        color: #777;
+        background-color: #fff;
+        -webkit-text-size-adjust: 100%;
+        -webkit-tap-highlight-color: rgba(0,0,0,0);
+    }
+
+    *, *::before, *::after {
+        box-sizing: border-box;
+    }
+
+    @media (min-width: 1200px){
+    h1, .h1 {
+        font-size: 2.5rem;
+    }}
+
+    @media (min-width: 1200px){
+    h3, .h3 {
+        font-size: 1.75rem;
+    }}
+
+</style>
+
+</head>
+
+<body>
+
+    <div class="d-flex justify-content-between p-3" >
+        <img src="img/logo.png" width="150">
+        <div class="text-center" >
+            <h5>MARIO ALBERTO VERDUZCO COTA</h5>
+            <h5>VECM880923NI1</h5>
+            <p>SANTOS DEGOLLADO 451 CENTRO LOS MOCHIS</p>
+            <P>SINALOA MEXICO CP.81200</P>
+            <P>TEL.8123595 ID 42*15*540025</P>
+        </div>
+        <div class="text-center" >
+            <h3>Estado de cuenta</h3>
+            <?php date_default_timezone_set('America/Mexico_City'); ?>
+            <P><?php echo "Fecha: " . date('d/m/Y') ?></P>
+            <P><?php echo "Hora: " . date('h:i:s A') ?></P>
+        </div>
+    </div>
+
+    <div class="d-flex justify-content-center p-3" >
+        <div class="text-center" >
+            <h5><?php echo $datos['cliente']['RFC'] ?></h5>
+            <p><?php echo $datos['cliente']['Clave'] . " " . $datos['cliente']['Razon_Social'] ?></p>
+            <P><?php echo
+                $datos['cliente']['Domicilio'] . " " .
+                $datos['cliente']['Num_Exterior']  . ", " .
+                $datos['cliente']['Num_interior']  . " " .
+                $datos['cliente']['Colonia']  . " " .
+                $datos['cliente']['Municipio']  . " " .
+                $datos['cliente']['Estado']  . ", " .
+                $datos['cliente']['Ciudad']  . " " .
+                $datos['cliente']['Pais']  . " C.P. " .
+                $datos['cliente']['Codigo_Postal']
+            ?></P>
+        </div>
+    </div>
+
+    <div class="p-1">
+    <table class="table mt-1">
+      <thead>
+        <tr>
+          <th>Folio</th>
+          <th>Inicio</th>
+          <th>Vencimiento</th>
+          <th>Importe</th>
+          <th>Abono</th>
+          <th>Debe</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        
+            $preparada = $datos['conexion_base_sucursal']->prepare("SELECT Pagos.Fecha, Pagos.FechaVencimiento, Pagos.Importe, Pagos.Abono, Pagos.Folio AS FolioComprobante, PedidosCliente.Folio AS Folio FROM Pagos INNER JOIN PedidosCliente ON PedidosCliente.FolioComprobante = Pagos.Folio AND PedidosCliente.Cliente = :clienteTemp AND PedidosCliente.Status != 'CA' WHERE Pagos.Cliente = :cliente AND Pagos.Saldado = 0 ORDER BY Pagos.Fecha");
+            $preparada->bindValue(':clienteTemp', $datos['cliente']['Clave']);
+            $preparada->bindValue(':cliente', $datos['cliente']['Clave']);
+            $preparada->execute();
+
+            $importes = 0;
+            $abonos = 0;
+
+            foreach( $preparada->fetchAll(PDO::FETCH_ASSOC) as $factura ){
+                $importes += $factura["Importe"];
+                $abonos += $factura["Abono"];
+                echo "<tr>". 
+                        "<td>" . $factura["FolioComprobante"] . "</td>".
+                        "<td>" . $factura["Fecha"] . "</td>".
+                        "<td>" . $factura["FechaVencimiento"] . "</td>".
+                        "<td class=\"dinero\" >" . $factura["Importe"] . "</td>".
+                        "<td class=\"dinero\" >" . $factura["Abono"] . "</td>".
+                        "<td class=\"dinero\" >" . $factura["Importe"] - $factura["Abono"] . "</td>".
+                    "</tr>";
+            }
+
+        ?>
+      </tbody>
+      <tfoot>
+        <tr>
+          <th></th>
+          <th></th>
+          <th>Totales:</th>
+          <th class="dinero"><?php echo $importes; ?></th>
+          <th class="dinero"><?php echo $abonos; ?></th>
+          <th class="dinero"><?php echo $importes - $abonos; ?></th>
+          <th></th>
+        </tr>
+    </tfoot>
+    </table>
+  </div>
+
+
+</body>
+
+</html>
+<?php
+    $html = ob_get_clean();
+
+    require_once 'dompdf/autoload.inc.php';
+
+    use Dompdf\Dompdf;
+    use Dompdf\Options;
+
+    $options = new Options();
+    // $options->set('isRemoteEnabled', true);
+    $options->set('chroot', __DIR__);
+    // $options->set('tempDir', 'tamporaldir');
+    // $options->set('isHtml5ParserEnabled', true);
+    // $options->set('isPhpEnabled', true);
+    // $options->set('debugPng', true);
+    // $options->set('debugCss', true);
+
+    $dompdf = new Dompdf($options);
+
+    $dompdf->loadHtml($html);
+
+    $dompdf->render();
+
+    $dompdf->stream("estado de cuenta.pdf", array("Attachment" => false));
+    //file_put_contents('filename.pdf', $dompdf->output());
+?>
