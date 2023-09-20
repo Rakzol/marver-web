d056ccf476ee2739d34ef13919413ebec285d0d2
Rakzol <carlosgilbertojr98@hotmail.com>
2023-09-20 13:16:01 -0700

    cambios

--- preventa.php
+++ preventa.php
@@ -122,10 +122,9 @@
             $preparada->bindValue(':folio', $_GET['folio']);
             $preparada->execute();
 
-            // $preventas_positivas = [];
             foreach( $preparada->fetchAll(PDO::FETCH_ASSOC) as $preventa ){
                 if( $preventa['Cantidad'] > 0 ){
-                    if( $preventas_positivas[$preventa['CodigoArticulo']] ){
+                    if( isset($preventas_positivas[$preventa['CodigoArticulo']]) ){
                         $preventas_positivas[$preventa['CodigoArticulo']]['Cantidad'] += $preventa['Cantidad'];
                     }else{
                         $preventas_positivas[$preventa['CodigoArticulo']] = $preventa;
