693a53aeaa15af2d13d712a1e660ddd3f75c2471
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2023-11-30 17:28:20 -0600

    cambios

--- factura_pdf.php
+++ factura_pdf.php
@@ -137,6 +137,7 @@
         <div class="aliniacion-vertical" >
             <h3>INGRESO FACTURA ORIGINAL</h3>
             <h3 class="linea" >Fecha: </h3><p class="linea" ><?php echo $datos_factura_electronica['Fecha'] . ' ' . $datos_factura_electronica['Hora'] ?></p>
+            <br>
             <h3 class="linea me-10" >Serie <?php echo $datos_factura_electronica['Serie'] ?> </h3><h3 class="linea" >Folio <?php echo $_GET['folio_comprobante'] ?></h3>
             <h3>Folio Fiscal</h3>
             <p class="linea" ><?php echo $xml->xpath('//tfd:TimbreFiscalDigital')[0]['UUID'] ?></p>
