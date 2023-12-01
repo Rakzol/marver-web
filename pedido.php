cb18a15c58b3a6938ae9c98d2a4beb54b493cc5c
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2023-11-30 19:48:37 -0600

    cambios

--- pedido.php
+++ pedido.php
@@ -406,7 +406,7 @@
 </head>
 
 <body>
-
+<br><br><br><br><br><br>
     <!-- Spinner Start -->
     <div id="spinner" manual='si' 
         class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
