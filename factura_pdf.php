cc29d3698538140dc8e3295acb084833136ae739
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2023-11-30 20:02:44 -0600

    cambios

--- factura_pdf.php
+++ factura_pdf.php
@@ -228,7 +228,7 @@
             <div><h3 class="linea" >Total: </h3><p class="linea" ><?php echo number_format((float)$xml->xpath('//cfdi:Comprobante')[0]['Total'], 2, '.', ',') ?></p></div>
             <?php QRcode::png( "?re=" . $xml->xpath('//cfdi:Emisor')[0]['Rfc'] . 
                                 "&rr=" . $xml->xpath('//cfdi:Receptor')[0]['Rfc'] . 
-                                "&tt=" ( str_pad( explode('.',$xml->xpath('//cfdi:Comprobante')[0]['Total'])[0], 10, '0', STR_PAD_LEFT ) . '.' . str_pad( explode('.',$xml->xpath('//cfdi:Comprobante')[0]['Total'])[1], 6, '0', STR_PAD_RIGHT ) ) . 
+                                "&tt=" . ( str_pad( explode('.',$xml->xpath('//cfdi:Comprobante')[0]['Total'])[0], 10, '0', STR_PAD_LEFT ) . '.' . str_pad( explode('.',$xml->xpath('//cfdi:Comprobante')[0]['Total'])[1], 6, '0', STR_PAD_RIGHT ) ) . 
                                 "&id=" . $xml->xpath('//tfd:TimbreFiscalDigital')[0]['UUID'] , "qr/" . $xml->xpath('//tfd:TimbreFiscalDigital')[0]['UUID'] . ".png"); ?>
             <img width="150" height="150" style="background: red;" src="<?php echo "qr/" . $xml->xpath('//tfd:TimbreFiscalDigital')[0]['UUID'] . ".png" ?>" alt="">
 
