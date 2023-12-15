c376e7da52c66358a19f870bbdbe9f2a7065ac69
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2023-12-15 15:08:06 -0600

    cambios

---
+++ android/posiciones.php
@@ -0,0 +1,28 @@
+<?php
+    try{
+        header('Content-Type: application/json');
+
+        $conexion = new PDO('sqlsrv:Server=10.10.10.130;Database=Mochis;TrustServerCertificate=true','MARITE','2505M$RITE');
+        $conexion->setAttribute(PDO::SQLSRV_ATTR_FETCHES_NUMERIC_TYPE, True);
+
+        $preparada = $conexion->prepare('SELECT Clave FROM Vendedores WHERE Nombre = :usuario AND Contraseña = :contrasena');
+        $preparada->bindValue(':usuario', $_POST['usuario']);
+        $preparada->bindValue(':contrasena', $_POST['contraseña']);
+        $preparada->execute();
+
+        $usuarios = $preparada->fetchAll(PDO::FETCH_ASSOC);
+
+        if( count($usuarios) == 0 ){
+            $resultado["status"] = 1;
+            echo json_encode($resultado);
+            exit();
+        }
+
+        $preparada = $conexion->prepare('SELECT usuario, latitud, longitud FROM ( SELECT usuario, latitud, longitud, ROW_NUMBER() OVER ( PARTITION BY usuario ORDER BY fecha DESC) AS fila FROM posiciones ) AS ordenado WHERE fila = 1');
+        $preparada->execute();
+
+        echo json_encode($preparada->fetchAll(PDO::FETCH_ASSOC), JSON_UNESCAPED_UNICODE);
+    }catch( Exception $exception ) {
+        header('HTTP/1.1 500 ' . $exception->getMessage());
+    }
+?>
