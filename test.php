0107f74914417a08197bc3bcbbb565e2874aa504
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-01-13 16:15:02 -0600

    cambios

--- test.php
+++ test.php
@@ -17,6 +17,11 @@
         $preparada->bindValue(':dia_final', '2050-12-28');
         $preparada->execute();
 
+        $velocidad_limite = 5;
+        $segundos_inicio = 600;
+        $segundos_fin = 3;
+
+        $resultados = [];
         foreach( $preparada->fetchAll(PDO::FETCH_ASSOC) as $repartidor ){
             print_r($repartidor);
 
@@ -24,10 +29,34 @@
             $preparada->bindValue(':repartidor', $repartidor['usuario']); 
             $preparada->execute();
 
-            foreach( $preparada->fetchAll(PDO::FETCH_ASSOC) as $posicion ){
-                if( $posicion['id'] % 10000 == 0 ){
-                    print_r($posicion);
+            $posicion_mala = [];
+            $posicion_buena = [];
+
+            $posiciones = $preparada->fetchAll(PDO::FETCH_ASSOC);
+            foreach( $posiciones as $posicion ){
+
+                if(!$posicion_mala){
+                    if( $posicion['velocidad'] <= $velocidad_limite ){
+                        $posicion_mala = $posicion;
+                    }
+                }else{
+                    if(!$posicion_buena){
+                        if( $posicion['velocidad'] > $velocidad_limite ){
+                            $posicion_buena = $posicion;
+                        }
+                    }else{
+                        if( $posicion['velocidad'] <= $velocidad_limite ){
+                            $posicion_buena = [];
+                        }else{
+                            if( new DateTime($posicion_buena['fecha'])->getTimestamp() - new DateTime($posicion['fecha'])->getTimestamp() >= $segundos_fin ){
+                                if( new DateTime($posicion_mala['fecha'])->getTimestamp() - new DateTime($posicion_mala['fecha'])->getTimestamp() >= $segundos_inicio ){
+                                    print_r( [$posicion_mala,$posicion_mala] );
+                                }
+                            }
+                        }
+                    }
                 }
+                
             }
 
         }
