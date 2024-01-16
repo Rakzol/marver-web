67b03b051b6d49952fcf66247dc19ba0eb271e85
Rakzol <carlosgilbertojr98@hotmail.com>
2024-01-16 03:49:19 -0600

    cambios

--- Excesos.php
+++ Excesos.php
@@ -412,6 +412,12 @@
 
     <div class="container mt-3 p-3 bg-white rounded">
         <h1 class="text-center" >Excesos</h1>
+
+        <div class="form-floating mb-3">
+  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+  <label for="floatingInput">Email address</label>
+</div>
+
         <!-- dia minutos limite de reposo velocidad limite de conduccion -->
         <table class="table mb-0">
         <thead>
