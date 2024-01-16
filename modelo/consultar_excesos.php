46193a22439ee854f040f039f43fea1768c05cf1
Rakzol <carlosgilbertojr98@hotmail.com>
2024-01-16 08:50:05 -0600

    cambios

--- modelo/consultar_excesos.php
+++ modelo/consultar_excesos.php
@@ -19,7 +19,7 @@
         $preparada->bindValue(':dia_final', $_POST['fecha']);
         $preparada->execute();
 
-        $segundos_fin = 10;
+        $segundos_fin = 60;
 
         $resultados = [];
         foreach( $preparada->fetchAll(PDO::FETCH_ASSOC) as $repartidor ){
