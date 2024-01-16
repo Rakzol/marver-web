2a02f35f758f4b280cf17c43f135b803923edabb
Rakzol <carlosgilbertojr98@hotmail.com>
2024-01-16 03:40:31 -0600

    cambios

--- Excesos.php
+++ Excesos.php
@@ -402,7 +402,7 @@
 <body>
 
     <!-- Spinner Start -->
-    <div id="spinner" manual = 'si'
+    <div id="spinner" manual = 'no'
         class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
         <div class="spinner-border position-relative text-primary" style="width: 6rem; height: 6rem;" role="status">
         </div>
