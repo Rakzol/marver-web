c799cdfcbb72820a9ab4e3c21627cb7be5042242
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-01-18 15:37:01 -0600

    cambios

--- modelo/consultar_excesos.php
+++ modelo/consultar_excesos.php
@@ -19,7 +19,7 @@
         $preparada->bindValue(':dia_final', $_POST['fecha']);
         $preparada->execute();
 
-        $segundos_fin = 300;
+        $segundos_fin = 180;
 
         $resultados = [];
         foreach( $preparada->fetchAll(PDO::FETCH_ASSOC) as $repartidor ){
