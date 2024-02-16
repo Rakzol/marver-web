abb779aa0e391821d76489c4900c8b4ddbeab28d
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-02-16 17:10:28 -0600

    cambios

--- android/posicion.php
+++ android/posicion.php
@@ -6,8 +6,8 @@
         $conexion->setAttribute(PDO::SQLSRV_ATTR_FETCHES_NUMERIC_TYPE, True);
 
         $preparada = $conexion->prepare('SELECT Clave FROM Vendedores WHERE Nombre = :usuario AND Contraseña = :contrasena');
-        $preparada->bindValue(':usuario', $_POST['u']);
-        $preparada->bindValue(':contrasena', $_POST['c']);
+        $preparada->bindValue(':usuario', $_GET['u']);
+        $preparada->bindValue(':contrasena', $_GET['c']);
         $preparada->execute();
 
         $usuarios = $preparada->fetchAll(PDO::FETCH_ASSOC);
@@ -20,9 +20,9 @@
 
         $preparada = $conexion->prepare('INSERT INTO posiciones VALUES( :usuario, :latitud, :longitud, :velocidad, GETDATE() )');
         $preparada->bindValue(':usuario', $usuarios[0]['Clave']);
-        $preparada->bindValue(':latitud', $_POST['la']);
-        $preparada->bindValue(':longitud', $_POST['ln']);
-        $preparada->bindValue(':velocidad', $_POST['v']);
+        $preparada->bindValue(':latitud', $_GET['la']);
+        $preparada->bindValue(':longitud', $_GET['ln']);
+        $preparada->bindValue(':velocidad', $_GET['v']);
         $preparada->execute();
 
     }catch( Exception $exception ) {
