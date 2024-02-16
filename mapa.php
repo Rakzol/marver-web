baf1cf0f817a96f9fdf8fcdc713eff0001ebaf4f
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-02-15 18:02:33 -0600

    cambios

--- mapa.php
+++ mapa.php
@@ -333,6 +333,8 @@
                 mapId: '7845e7dffe8cea37'
             });
 
+            mapa.setCenter({'lat':25.800006,'lng':-108.999742});
+
             setInterval(actualizacion_logica, 500);
             setTimeout(procesar_vista, 10);
 
