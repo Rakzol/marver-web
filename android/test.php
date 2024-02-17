803bf1a28d394205c2732026e48206ce86f76cf5
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-02-16 18:33:07 -0600

    cambios

--- android/test.php
+++ android/test.php
@@ -1,8 +1,3 @@
 <?php
-    header_remove('Content-Type');
-    header_remove('Server');
-    header_remove('X-Powered-By');
-    header_remove('MARVER Refacciones');
-    header_remove('Date');
-    header_remove('Content-Length');
+    //header_remove('X-Powered-By');
 ?>
