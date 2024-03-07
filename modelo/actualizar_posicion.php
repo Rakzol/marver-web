531fd2043edc57eaee2e738b7ce36ae105ac7b39
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-03-06 18:57:02 -0700

    cambios

--- modelo/actualizar_posicion.php
+++ modelo/actualizar_posicion.php
@@ -4,20 +4,24 @@
 
         //Intentamos agregar una unidad extra al producto en carrito del usuario actual
         $preparada = $datos['conexion_catalogo_sucursal']->prepare("
-        IF EXISTS (SELECT 1 FROM clientes_posiciones WHERE clave = :clave)
+        IF EXISTS (SELECT 1 FROM clientes_posiciones WHERE clave = :clave_1)
         BEGIN
             UPDATE clientes_posiciones
-            SET latitud = :latitud,
-                longitud = :longitud
-            WHERE clave = :clave;
+            SET latitud = :latitud_1,
+                longitud = :longitud_1
+            WHERE clave = :clave_2;
         END
         ELSE
         BEGIN
-            INSERT INTO clientes_posiciones (clave, latitud, longitud) VALUES (:clave, :latitud, :longitud);
+            INSERT INTO clientes_posiciones (clave, latitud, longitud) VALUES (:clave_3, :latitud_2, :longitud_2);
         END");
-        $preparada->bindValue(':clave', $datos['usuario']['cliente']);
-        $preparada->bindValue(':latitud', $_POST['latitud']);
-        $preparada->bindValue(':longitud', $_POST['longitud']);
+        $preparada->bindValue(':clave_1', $datos['usuario']['cliente']);
+        $preparada->bindValue(':clave_2', $datos['usuario']['cliente']);
+        $preparada->bindValue(':clave_3', $datos['usuario']['cliente']);
+        $preparada->bindValue(':latitud_1', $_POST['latitud']);
+        $preparada->bindValue(':longitud_1', $_POST['longitud']);
+        $preparada->bindValue(':latitud_2', $_POST['latitud']);
+        $preparada->bindValue(':longitud_2', $_POST['longitud']);
         $preparada->execute();
 
         $resultado["actualizada"] = true;
