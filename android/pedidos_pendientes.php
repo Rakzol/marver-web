64e2d66a86e4c7f22817683565be1125b4e6a6ed
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-03-13 17:47:17 -0700

    cambios

--- android/pedidos_pendientes.php
+++ android/pedidos_pendientes.php
@@ -34,13 +34,16 @@
 			clientes_posiciones.numero_exterior AS numero_exterior,
 			clientes_posiciones.numero_interior AS numero_interior,
 			clientes_posiciones.observaciones AS observaciones,
-			clientes_posiciones.calle AS calle
+			clientes_posiciones.calle AS calle,
+			MoviemientosVenta.Importe * -1 AS feria,
+			MoviemientosVenta.Feria + (MoviemientosVenta.Importe * -1) AS regreso
             FROM
             EnvioPedidoCliente
             INNER JOIN PedidosCliente ON PedidosCliente.Folio = EnvioPedidoCliente.Pedido
             INNER JOIN Clientes ON Clientes.Clave = PedidosCliente.Cliente
             INNER JOIN Ventas ON Ventas.Folio = PedidosCliente.FolioComprobante AND Ventas.TipoComprobante = PedidosCliente.Tipocomprobante
             LEFT JOIN clientes_posiciones ON clientes_posiciones.clave = PedidosCliente.Cliente
+            LEFT JOIN MoviemientosVenta ON MoviemientosVenta.Folio = PedidosCliente.FolioComprobante AND MoviemientosVenta.TipoComprobante = 11 AND MoviemientosVenta.Importe < 0
             WHERE
             EnvioPedidoCliente.Fecha = CONVERT(DATE, GETDATE())
             AND Ventas.Status = 3
