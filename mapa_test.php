58a249f3750c55cd114fda3e258366053f29c829
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-03-16 15:15:34 -0700

    cambios

--- mapa_test.php
+++ mapa_test.php
@@ -358,7 +358,7 @@
                                     }
                                     /* fin: Marcadores y polilineas secundarias */
 
-                                    mapa.fitBounds(latitud_longitud_limite);
+                                    mapa.fitBounds(latitud_longitud_limite,150);
 
                                     consultar_pedidos = false;
                                     consultas_polilineas -= 1;
