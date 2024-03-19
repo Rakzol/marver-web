dbbfec4de4bce8e67889d5414eb870dca2122e71
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-03-19 13:33:18 -0700

    cambios

--- mapa_test.php
+++ mapa_test.php
@@ -173,7 +173,7 @@
                         let datos_envio = new FormData();
                         datos_envio.append('clave',fijado);
 
-                        fetch('android/pedidos_en_ruta_test', {
+                        fetch('android/pedidos_en_ruta', {
                             method: "POST",
                             body: datos_envio
                         })
