789818fa7e3fe6824960de7487f2604dfdfc0c01
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-03-25 18:51:11 -0700

    cambios

---
+++ android/ruta_repartidor.php
@@ -0,0 +1,33 @@
+<?php
+    try{
+        header('Content-Type: application/json');
+
+        $conexion = new PDO('sqlsrv:Server=10.10.10.130;Database=Mochis;TrustServerCertificate=true','MARITE','2505M$RITE');
+        $conexion->setAttribute(PDO::SQLSRV_ATTR_FETCHES_NUMERIC_TYPE, True);
+
+        $preparada = $conexion->prepare('SELECT TOP 1 id FROM rutas_repartidores WHERE repartidor = :repartidor AND fecha_inicio IS NULL');
+        $preparada->bindValue(':repartidor', $_POST['clave']);
+        $preparada->execute();
+
+        $rutas_repartidores = $preparada->fetchAll(PDO::FETCH_ASSOC);
+        if(count($rutas_repartidores) == 0){
+            $resultado["status"] = 1;
+            $resultado["mensaje"] = "No tiene rutas en curso";
+            echo json_encode($resultado);
+        }else{
+            $id_ruta_reparto = $rutas_repartidores[0]['id'];
+        }
+
+        $resultado["status"] = 0;
+        $resultado["mensaje"] = $id_ruta_reparto;
+        echo json_encode($resultado);
+
+        // echo json_encode($preparada->fetchAll(PDO::FETCH_ASSOC), JSON_UNESCAPED_UNICODE);
+    }catch( Exception $exception ) {
+        // header('HTTP/1.1 500 ' . $exception->getMessage());
+
+        $resultado["status"] = 69;
+        $resultado["mensaje"] = "Error al obtener la ruta del repartidor";
+        echo json_encode($resultado);
+    }
+?>
