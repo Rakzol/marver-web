3a82333c4bcba43288b733b2efb91292659f2580
Rakzol <carlosgilbertojr98@hotmail.com>
2024-03-30 18:04:18 -0700

    cambios

--- android/asignar_pedido_test.php
+++ android/asignar_pedido_test.php
@@ -6,8 +6,8 @@
         $conexion->setAttribute(PDO::SQLSRV_ATTR_FETCHES_NUMERIC_TYPE, True);
 
         $preparada = $conexion->prepare('SELECT Clave FROM Vendedores WHERE Clave = :clave AND Contraseña = :contrasena');
-        $preparada->bindValue(':clave', $_POST['clave']);
-        $preparada->bindValue(':contrasena', $_POST['contraseña']);
+        $preparada->bindValue(':clave', $_GET['clave']);
+        $preparada->bindValue(':contrasena', $_GET['contraseña']);
         $preparada->execute();
 
         $usuarios = $preparada->fetchAll(PDO::FETCH_ASSOC);
@@ -19,8 +19,8 @@
             exit();
         }
 
-        $preparada = $conexion->prepare('SELECT TOP 1 id FROM rutas_repartidores WHERE repartidor = :repartidor AND fecha_inicio IS NOT NULL');
-        $preparada->bindValue(':repartidor', $_POST['clave']);
+        $preparada = $conexion->prepare('SELECT TOP 1 id FROM rutas_repartidores WHERE repartidor = :repartidor AND fecha_inicio IS NOT NULL AND fecha_fin IS NULL');
+        $preparada->bindValue(':repartidor', $_GET['clave']);
         $preparada->execute();
 
         if( count($preparada->fetchAll(PDO::FETCH_ASSOC)) > 0 ){
@@ -31,32 +31,32 @@
         }
 
         $preparada = $conexion->prepare("SELECT PedidosCliente.Folio, EnvioPedidoCliente.Responsable FROM PedidosCliente LEFT JOIN EnvioPedidoCliente ON EnvioPedidoCliente.Pedido = PedidosCliente.Folio WHERE PedidosCliente.Folio = :folio");
-        $preparada->bindValue(':folio', $_POST['folio']);
+        $preparada->bindValue(':folio', $_GET['folio']);
         $preparada->execute();
 
         $pedido = $preparada->fetchAll(PDO::FETCH_ASSOC);
         if( count($pedido) == 0 && false ){
             $resultado["status"] = 3;
-            $resultado["mensaje"] = "El pedido con el folio: " . $_POST['folio'] . " no existe";
+            $resultado["mensaje"] = "El pedido con el folio: " . $_GET['folio'] . " no existe";
             echo json_encode($resultado);
             exit();
         }
 
         if( $pedido[0]['Responsable'] != NULL && false ){
             $resultado["status"] = 4;
-            $resultado["mensaje"] = "El pedido con el folio: " . $_POST['folio'] . " ya esta asignado al repartidor: " . $pedido[0]['Responsable'];
+            $resultado["mensaje"] = "El pedido con el folio: " . $_GET['folio'] . " ya esta asignado al repartidor: " . $pedido[0]['Responsable'];
             echo json_encode($resultado);
             exit();
         }
 
         $preparada = $conexion->prepare('SELECT TOP 1 id FROM rutas_repartidores WHERE repartidor = :repartidor AND fecha_inicio IS NULL');
-        $preparada->bindValue(':repartidor', $_POST['clave']);
+        $preparada->bindValue(':repartidor', $_GET['clave']);
         $preparada->execute();
 
         $rutas_repartidores = $preparada->fetchAll(PDO::FETCH_ASSOC);
         if(count($rutas_repartidores) == 0){
             $preparada = $conexion->prepare('INSERT INTO rutas_repartidores (repartidor) VALUES (:repartidor)');
-            $preparada->bindValue(':repartidor', $_POST['clave']);
+            $preparada->bindValue(':repartidor', $_GET['clave']);
             $preparada->execute();
 
             $id_ruta_reparto = $conexion->lastInsertId();
@@ -66,16 +66,16 @@
 
         $preparada = $conexion->prepare('INSERT INTO pedidos_repartidores VALUES (:ruta_repartidor,:folio)');
         $preparada->bindValue(':ruta_repartidor', $id_ruta_reparto);
-        $preparada->bindValue(':folio', $_POST['folio']);
+        $preparada->bindValue(':folio', $_GET['folio']);
         $preparada->execute();
 
         $preparada = $conexion->prepare("INSERT INTO EnvioPedidoClienteTest (Pedido, Responsable, Fecha, HoraEnvio) VALUES (:folio, :responsable, FORMAT(GETDATE(), 'yyyy-MM-dd'), REPLACE( REPLACE( FORMAT(GETDATE(), 'hh:mm:ss tt'), 'PM', 'p. m.' ), 'AM', 'a. m.' ) )");
-        $preparada->bindValue(':folio', $_POST['folio']);
-        $preparada->bindValue(':responsable', $_POST['clave']);
+        $preparada->bindValue(':folio', $_GET['folio']);
+        $preparada->bindValue(':responsable', $_GET['clave']);
         $preparada->execute();
 
         $resultado["status"] = 0;
-        $resultado["mensaje"] = "El pedido con el folio: " . $_POST['folio'] . " se asigno correctamente";
+        $resultado["mensaje"] = "El pedido con el folio: " . $_GET['folio'] . " se asigno correctamente";
         echo json_encode($resultado);
 
         // echo json_encode($preparada->fetchAll(PDO::FETCH_ASSOC), JSON_UNESCAPED_UNICODE);
@@ -83,7 +83,7 @@
         // header('HTTP/1.1 500 ' . $exception->getMessage());
 
         $resultado["status"] = 5;
-        $resultado["mensaje"] = "El pedido con el folio: " . $_POST['folio'] . " no es valido";
+        $resultado["mensaje"] = "El pedido con el folio: " . $_GET['folio'] . " no es valido";
         echo json_encode($resultado);
     }
 ?>
---
+++ android/iniciar_ruta_test.php
@@ -0,0 +1,70 @@
+<?php
+    try{
+        header('Content-Type: application/json');
+
+        $conexion = new PDO('sqlsrv:Server=10.10.10.130;Database=Mochis;TrustServerCertificate=true','MARITE','2505M$RITE');
+        $conexion->setAttribute(PDO::SQLSRV_ATTR_FETCHES_NUMERIC_TYPE, True);
+
+        $preparada = $conexion->prepare('SELECT Clave FROM Vendedores WHERE Clave = :clave AND Contraseña = :contrasena');
+        $preparada->bindValue(':clave', $_GET['clave']);
+        $preparada->bindValue(':contrasena', $_GET['contraseña']);
+        $preparada->execute();
+
+        $usuarios = $preparada->fetchAll(PDO::FETCH_ASSOC);
+
+        if( count($usuarios) == 0 ){
+            $resultado["status"] = 1;
+            $resultado["mensaje"] = "El vendedor no existe";
+            echo json_encode($resultado);
+            exit();
+        }
+
+        $preparada = $conexion->prepare('SELECT TOP 1 id FROM rutas_repartidores WHERE repartidor = :repartidor AND fecha_inicio IS NOT NULL AND fecha_fin IS NULL');
+        $preparada->bindValue(':repartidor', $_GET['clave']);
+        $preparada->execute();
+
+        if( count($preparada->fetchAll(PDO::FETCH_ASSOC)) > 0 ){
+            $resultado["status"] = 2;
+            $resultado["mensaje"] = "Tienes entregas en proceso";
+            echo json_encode($resultado);
+            exit();
+        }
+
+        $preparada = $conexion->prepare('SELECT TOP 1 id FROM rutas_repartidores WHERE repartidor = :repartidor AND fecha_inicio IS NULL AND fecha_fin IS NULL');
+        $preparada->bindValue(':repartidor', $_GET['clave']);
+        $preparada->execute();
+
+        $rutas_repartidores = $preparada->fetchAll(PDO::FETCH_ASSOC);
+        if( count($rutas_repartidores) == 0 ){
+            $resultado["status"] = 3;
+            $resultado["mensaje"] = "No tiene entregas pendientes de inicializacion";
+            echo json_encode($resultado);
+            exit();
+        }
+        $ruta_repartidor = $rutas_repartidores[0];
+
+        $preparada = $conexion->prepare("SELECT * FROM pedidos_repartidores WHERE ruta_repartidor = :ruta_repartidor");
+        $preparada->bindValue(':ruta_repartidor', $ruta_repartidor['id']);
+        $preparada->execute();
+
+        $pedido = $preparada->fetchAll(PDO::FETCH_ASSOC);
+        if( count($pedido) == 0 && false ){
+            $resultado["status"] = 3;
+            $resultado["mensaje"] = "El pedido con el folio: " . $_GET['folio'] . " no existe";
+            echo json_encode($resultado);
+            exit();
+        }
+
+        $resultado["status"] = 0;
+        $resultado["mensaje"] = "El pedido con el folio: " . $_GET['folio'] . " se asigno correctamente";
+        echo json_encode($resultado);
+
+        // echo json_encode($preparada->fetchAll(PDO::FETCH_ASSOC), JSON_UNESCAPED_UNICODE);
+    }catch( Exception $exception ) {
+        // header('HTTP/1.1 500 ' . $exception->getMessage());
+
+        $resultado["status"] = 5;
+        $resultado["mensaje"] = "Error al inicializar la ruta";
+        echo json_encode($resultado);
+    }
+?>
