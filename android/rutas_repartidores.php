b434c81870c1ace52a6e02eee4b8e26e55a5457d
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-04-04 16:59:15 -0700

    cambios

--- android/rutas_repartidores.php
+++ android/rutas_repartidores.php
@@ -177,7 +177,7 @@
 
         //echo json_encode($resultado);
 
-        echo str_replace('\\', '\\\\', $leg['polyline']['encodedPolyline']);
+        echo $leg['polyline']['encodedPolyline'];
         /*foreach( decodePolyline(normalizarEncodedPolyline($leg['polyline']['encodedPolyline'])) as $point ){
             echo '[' . $point[1] . ',' . $point[0] . '],';
         }*/
