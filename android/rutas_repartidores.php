600d788b5e2e057ecaec5ca798e1b4f930b921a7
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-04-03 18:12:48 -0700

    cambios

--- android/rutas_repartidores.php
+++ android/rutas_repartidores.php
@@ -171,7 +171,7 @@
             $leg = $resultado['ruta']['routes'][0]['legs'][count($resultado['ruta']['routes'][0]['legs'])-1];
         }
 
-        echo $leg;
+        echo json_encode($leg);
 
         echo json_encode($resultado);
         
