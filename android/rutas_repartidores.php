86229c6ce760830074975ea3beae942ecb7ceddc
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-04-06 15:23:47 -0700

    cambios

--- android/rutas_repartidores.php
+++ android/rutas_repartidores.php
@@ -201,7 +201,7 @@
         $resultado['dentro'] = \GeometryLibrary\PolyUtil::isLocationOnPath(
             ['lat' => $repartidor_seguido['lat'], 'lng' => $repartidor_seguido['lon']],
             $leg['polyline']['decodedPolyline'],
-            0.002
+            0.02
         );
 
         /*$menor_distancia = INF;
