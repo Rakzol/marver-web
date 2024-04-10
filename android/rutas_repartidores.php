e1eb6470074a0b53296256c58b2be05f003fdaac
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-04-09 17:15:50 -0700

    cambios

--- android/rutas_repartidores.php
+++ android/rutas_repartidores.php
@@ -106,6 +106,7 @@
                     $resultado['repartidor'] = array(
                         "id" => $repartidor_seguido['id'],
                         "nombre" => $repartidor_seguido['nombre'],
+                        "velocidad" => $repartidor_seguido['velocidad'],
                         "tipo" => "camino",
                         "color" => "#00000000",
                         "polilinea" => polilinea_ors($repartidor_seguido['lon'], $repartidor_seguido['lat'], $posiciones_repartidor[0]['longitud'], $posiciones_repartidor[0]['latitud'])['features'][0]['geometry']['coordinates']
@@ -116,6 +117,7 @@
                     $resultado['repartidor'] = array(
                         "id" => $repartidor_seguido['id'],
                         "nombre" => $repartidor_seguido['nombre'],
+                        "velocidad" => $repartidor_seguido['velocidad'],
                         "tipo" => "cercano",
                         "color" => "#00000000",
                         "polilinea" => array(
@@ -230,6 +232,7 @@
                 $resultado['repartidor'] = array(
                     "id" => $repartidor_seguido['id'],
                     "nombre" => $repartidor_seguido['nombre'],
+                    "velocidad" => $repartidor_seguido['velocidad'],
                     "tipo" => "camino",
                     "color" => $color,
                     "polilinea" => polilinea_ors($repartidor_seguido['lon'], $repartidor_seguido['lat'], $posiciones_repartidor[0]['longitud'], $posiciones_repartidor[0]['latitud'] )['features'][0]['geometry']['coordinates']
@@ -240,6 +243,7 @@
                 $resultado['repartidor'] = array(
                     "id" => $repartidor_seguido['id'],
                     "nombre" => $repartidor_seguido['nombre'],
+                    "velocidad" => $repartidor_seguido['velocidad'],
                     "tipo" => "cercano",
                     "color" => "#00000000",
                     "polilinea" => array(
@@ -255,6 +259,7 @@
             $resultado['repartidor'] = array(
                 "id" => $repartidor_seguido['id'],
                 "nombre" => $repartidor_seguido['nombre'],
+                "velocidad" => $repartidor_seguido['velocidad'],
                 "tipo" => "llego",
                 "color" => "#00000000",
                 "polilinea" => array(
