469468b3741206191fe21c0b70eafce6c9bdd349
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-04-10 15:51:12 -0700

    cambios

--- mapa_test.php
+++ mapa_test.php
@@ -155,6 +155,7 @@
 
                         console.log({ lat: repartidor['polilinea'][0][1], lng: repartidor['polilinea'][0][0] });
                         marcadores.push(marcador);
+                        break;
                     } );
 
                 }else{
