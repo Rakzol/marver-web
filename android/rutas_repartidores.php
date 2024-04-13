58f2036a1143c3f2fb0bc2b3aed2f6dc445c850e
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-04-13 13:09:28 -0700

    cambios

--- android/rutas_repartidores.php
+++ android/rutas_repartidores.php
@@ -157,7 +157,7 @@
         $rutas = json_decode( str_replace('\\', '\\\\', $ruta_repartidor['ruta']), true);
 
         for( $c = 0; $c < count($rutas['routes'][0]['legs']); $c++ ){
-            echo 'len - >' . count($rutas['routes'][0]['legs'][$c]['polyline']['encodedPolyline']);
+            echo 'encodedPolyline - >' . $rutas['routes'][0]['legs'][$c]['polyline']['encodedPolyline'];
             $decodesPolylines = \GeometryLibrary\PolyUtil::decode2($rutas['routes'][0]['legs'][$c]['polyline']['encodedPolyline']);
             $rutas['routes'][0]['legs'][$c]['color'] = '#000000';
             $rutas['routes'][0]['legs'][$c]['polyline']['decodedPolyline'] = $decodesPolylines[0];
