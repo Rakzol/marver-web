bccf71091129200f3aaad1109c1fc4641e7e0305
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-04-13 13:38:14 -0700

    cambios

--- android/rutas_repartidores.php
+++ android/rutas_repartidores.php
@@ -310,7 +310,7 @@
             $resultado['ruta']['legs'][$c]['duration'] = number_format( substr($resultado['ruta']['legs'][$c]['duration'], 0, -1) / 60, 1 );
             $resultado['ruta']['legs'][$c]['distance'] = number_format( $resultado['ruta']['legs'][$c]['distanceMeters'] / 1000, 1 );
 
-            $fecha->modify('+' . $resultado['ruta']['legs'][$c]['duration'] . ' minutes');
+            $fecha->modify('+' . 60 . ' minutes');
             //echo '+' . $resultado['ruta']['legs'][$c]['duration'] . ' minutes';
             $resultado['ruta']['legs'][$c]['llegada'] = $fecha->format('h:i A');
 
