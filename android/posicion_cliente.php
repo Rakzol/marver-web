f0c3ce23c3d464019a031017c62bde40782cf987
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-04-20 15:15:39 -0700

    cambios

--- android/posicion_cliente.php
+++ android/posicion_cliente.php
@@ -2,6 +2,10 @@
     try{
         session_start();
 
+        require_once 'geometria/SphericalUtil.php';
+        require_once 'geometria/PolyUtil.php';
+        require_once 'geometria/MathUtil.php';
+
         header('Content-Type: application/json');
 
         $conexion = new PDO('sqlsrv:Server=10.10.10.130;Database=Mochis;TrustServerCertificate=true','MARITE','2505M$RITE');
