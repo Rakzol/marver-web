8661806ae033881dd1bd878decccafe2b97ef630
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-05-29 17:46:07 -0700

    cambios

--- dados.py
+++ dados.py
@@ -2,17 +2,20 @@ import random
 
 lows = 0
 wins = 0
+tiros = 0
 
 while True:
 
+    tiros += 1
+
     if random.randint(1,6) > 3:
         lows += 1
-        if lows == 200:
-            print('wins: ', wins, ' lows: ', lows)
+        if lows == 20:
+            print('wins: ', wins, ' lows: ', lows, 'tiros:', tiros)
             exit()
     else:
         if lows == 0:
             wins += 1
         lows = 0
 
-    print('wins: ', wins, ' lows: ', lows)
+    print('wins: ', wins, ' lows: ', lows, 'tiros:', tiros)
