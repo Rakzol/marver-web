9ba1c0698a69f530d0887c66bef5dc68e49d6093
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-05-29 18:35:38 -0700

    cambios

--- dados.py
+++ dados.py
@@ -1,5 +1,6 @@
 import random
 
+balance = 0
 lows = 0
 wins = 0
 tiros = 0
@@ -10,12 +11,16 @@ while True:
 
     if random.randint(1,6) > 3:
         lows += 1
-        if lows == 20:
-            print('wins: ', wins, ' lows: ', lows, 'tiros:', tiros)
-            exit()
+        if lows == 8:
+
+            balance += tiros * 50000 - 26233633
+
+            print('wins: ', wins, ' lows: ', lows, 'tiros:', tiros, 'balance: ', balance)
+
+            lows = 0
+            wins = 0
+            tiros = 0
     else:
         if lows == 0:
             wins += 1
         lows = 0
-
-    print('wins: ', wins, ' lows: ', lows, 'tiros:', tiros)
