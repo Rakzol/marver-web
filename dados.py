e03e58abff8b877b973715e0f637d9a17c5fec2e
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-05-29 18:41:56 -0700

    cambios

--- dados.py
+++ dados.py
@@ -11,9 +11,9 @@ while True:
 
     if random.randint(1,6) > 3:
         lows += 1
-        if lows == 8:
+        if lows == 14:
 
-            balance += tiros * 50000 - 26233633
+            balance += tiros * 50000 - 3408867720
 
             print('wins: ', wins, ' lows: ', lows, 'tiros:', tiros, 'balance: ', balance)
 
