ca401f3fc7b440d03d9ff4717e82aaf2a5fc67e5
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-08-06 14:54:11 -0700

    cambios

--- carrito2.php
+++ carrito2.php
@@ -707,7 +707,7 @@
     <div class="modal-dialog modal-dialog-centered modal-md">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="producto-modal-label">Limite de credito</h5>
+          <h5 class="modal-title" id="producto-modal-label">Inconveniente encontrado</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
         </div>
         <div class="modal-body">
