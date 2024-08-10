a27554c969b430503cd4026ee59bba67115dfec0
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-08-10 09:29:33 -0700

    cambios

--- modelo/finalizar_pedido.php
+++ modelo/finalizar_pedido.php
@@ -285,17 +285,30 @@
 
         try{
             /*$url = "https://ipinfo.io/{$ip}?token=a39ff8f192d166";
+
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+
             $response = curl_exec($ch);
-            curl_close($ch);
-            $data = json_decode($response, true);
 
-            $location = explode(',', $data['loc']);
+            if ($response != false) {
+                $data = json_decode($response, true);
 
-            $lat_api = $location[0];
-            $lon_api = $location[1];*/
+                $location = explode(',', $data['loc']);
+
+                $lat_api = $location[0];
+                $lon_api = $location[1];
+            }
+
+            $lat_api = null;
+            $lon_api = null;
+
+            curl_close($ch);*/
+
         }catch(Exception $ex){
             $lat_api = null;
             $lon_api = null;
--- modelo/iptest.php
+++ modelo/iptest.php
@@ -1,13 +1,30 @@
 <?php
 
     $ip = "187.134.127.101";
-
+    
     $url = "https://ipinfo.io/{$ip}?token=a39ff8f192d166";
+
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+
     $response = curl_exec($ch);
+
+    if ($response != false) {
+        $data = json_decode($response, true);
+
+        $location = explode(',', $data['loc']);
+
+        $lat_api = $location[0];
+        $lon_api = $location[1];
+    }
+
+    $lat_api = null;
+    $lon_api = null;
+
     curl_close($ch);
-    print_r($response);
 
 ?>
