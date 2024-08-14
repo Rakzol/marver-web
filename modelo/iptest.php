97a48ce52fc37e1565b2017833fcc0a79978f268
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-08-14 12:33:31 -0700

    cambios

---
+++ modelo/iptest.php
@@ -0,0 +1,34 @@
+<?php
+
+    $ip = "187.134.127.101";
+
+    $url = "https://ipinfo.io/{$ip}?token=a39ff8f192d166";
+
+    $curl = curl_init();
+    
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+
+    $response = curl_exec($curl);
+
+    if ($response != false) {
+        $data = json_decode($response, true);
+
+        $location = explode(',', $data['loc']);
+
+        $lat_api = $location[0];
+        $lon_api = $location[1];
+    }else{
+        $lat_api = null;
+        $lon_api = null;
+    }
+
+    curl_close($curl);
+
+
+
+    echo $lat_api;
+    echo $lon_api;
+?>
