f8923392940971ae3248a76cbb49c08ec469da67
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-08-19 15:30:44 -0700

    cambios

--- modelo/iniciar_sesion.php
+++ modelo/iniciar_sesion.php
@@ -38,6 +38,68 @@
         
         $_SESSION['usuario'] = $usuarios[0]['id'];
 
+        /********/
+        require_once('inicializar_datos.php');
+
+        $preparada = $datos['conexion_catalogo_sucursal']->prepare('DELETE FROM carrito WHERE usuario = :usuario');
+        $preparada->bindValue(':usuario', $datos['usuario']['id']);
+        $preparada->execute();
+
+        if( isset($_SERVER['HTTP_X_FORWARDED_FOR']) ){
+            $ip_list = explode(',',$_SERVER['HTTP_X_FORWARDED_FOR']);
+            $ip = trim($ip_list[0]);
+        }else{
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+
+        if( !filter_var($ip, FILTER_VALIDATE_IP) ){
+            $ip = '0.0.0.0';
+        }
+
+        try{
+            $url = "https://ipinfo.io/{$ip}?token=a39ff8f192d166";
+
+            $curl = curl_init();
+            
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+
+            $response = curl_exec($curl);
+
+            if ($response != false) {
+                $data = json_decode($response, true);
+
+                $location = explode(',', $data['loc']);
+
+                $lat_api = $location[0];
+                $lon_api = $location[1];
+            }else{
+                $lat_api = null;
+                $lon_api = null;
+            }
+
+            curl_close($curl);
+
+        }catch(Exception $ex){
+            $lat_api = null;
+            $lon_api = null;
+        }
+
+        $preparada = $datos['conexion_catalogo_principal']->prepare('INSERT INTO ubicaciones_usuarios VALUES (:usuario, :ip, :lat_nav, :lon_nav, :lat_api, :lon_api, :precision, GETDATE(), :pedido)');
+        $preparada->bindValue(':usuario', $datos['usuario']['id'] );
+        $preparada->bindValue(':ip', $ip);
+        $preparada->bindValue(':lat_nav', $_POST['lat_nav']);
+        $preparada->bindValue(':lon_nav', $_POST['lon_nav']);
+        $preparada->bindValue(':lat_api', $lat_api);
+        $preparada->bindValue(':lon_api', $lon_api);
+        $preparada->bindValue(':precision', $_POST['precision']);
+        $preparada->bindValue(':pedido', null);
+        $preparada->execute();
+
+        /***********/
+
         echo '{"correo": true, "contraseña": true}';
     }catch( Exception $exception ) {
         header('HTTP/1.1 500 ' . $exception->getMessage());
