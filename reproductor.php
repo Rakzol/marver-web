b4e9ef044bb9a5335ef18265c78ba5af24b8908b
Rakzol <carlosgilbertojr98@hotmail.com>
2024-11-03 15:23:07 -0700

    cambios

--- reproductor.php
+++ reproductor.php
@@ -299,7 +299,7 @@
 
             mapa = new Map(document.getElementById("mapa"), {
                 center: { lat: posicionActual()['latitud'], lng: posicionActual()['longitud'] },
-                zoom: 14,
+                zoom: 16,
                 mapId: '7845e7dffe8cea37',
                 mapTypeId: google.maps.MapTypeId.HYBRID
             });
@@ -318,7 +318,7 @@
             });
 
             marcador.addListener("click", () => {
-                mapa.setZoom(14);
+                mapa.setZoom(16);
                 mapa.setMapTypeId(google.maps.MapTypeId.HYBRID);
                 mapa.panTo(marcador.position);
 
