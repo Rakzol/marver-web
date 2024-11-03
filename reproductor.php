bb9d747640d758f4c3b27ae6e4ce6742e7ec365e
Rakzol <carlosgilbertojr98@hotmail.com>
2024-11-03 15:10:29 -0700

    cambios

--- reproductor.php
+++ reproductor.php
@@ -276,7 +276,9 @@
 
         async function posicionActual(){
             let posicion = posiciones.find(posicion=>new Date(posicion["fecha"])>=new Date(cursor.valueAsNumber));
-            if posicion return posicion;
+            if (posicion) {
+                return posicion
+            };
             return posiciones.at(-1)
         }
 
