41aa490537328e0167131e62632d64a40b72bc63
Rakzol <carlosgilbertojr98@hotmail.com>
2024-11-25 05:55:46 -0700

    cambios

--- reproductor.php
+++ reproductor.php
@@ -215,7 +215,7 @@
 
                 <div class="d-flex justify-content-center gap-3 mb-3" >
                     <h5 class="card-title mb-0 align-self-center" id="txtNombreRepartidor">Seleccione un Repartidor</h5>
-                    <strong id="txtIdRepartidor"></strong>
+                    <h5 id="txtIdRepartidor"></h5>
                     <p class="card-text mb-0 align-self-center" id="velocidadRepartidor">0.0 Km/h</p>
                     <!-- <div class="form-check form-switch d-inline-block align-self-center" >
                         <input class="form-check-input" id="seguirRepartidor" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
