13b8c486c89c68e48d40783b47c9cf67a373bbc7
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-11-28 13:32:19 -0700

    cambios

--- reproductor.php
+++ reproductor.php
@@ -10,7 +10,7 @@
         $conexion = new PDO('sqlsrv:Server=10.10.10.130;Database=Mochis;TrustServerCertificate=true','MARITE','2505M$RITE');
         $conexion->setAttribute(PDO::SQLSRV_ATTR_FETCHES_NUMERIC_TYPE, True);
 
-        $preparada = $conexion->prepare("SELECT * FROM posiciones WHERE fecha >= :dia_inicial AND fecha < DATEADD(DAY, 1, :dia_final) AND usuario = :repartidor ORDER BY fecha ASC");
+        $preparada = $conexion->prepare("SELECT latitud, longitud, velocidad FROM posiciones WHERE fecha >= :dia_inicial AND fecha < DATEADD(DAY, 1, :dia_final) AND usuario = :repartidor ORDER BY fecha ASC");
         $preparada->bindValue(':dia_inicial', $_GET['fecha']);
         $preparada->bindValue(':dia_final', $_GET['fecha']);
         $preparada->bindValue(':repartidor', $_GET['id']);
@@ -122,13 +122,20 @@
 
             $pedidos[$c]["entregas"] = $preparada->fetchAll(PDO::FETCH_ASSOC);
 
-            $pedidos[$c]["rutaRealizada"] = array_filter($posiciones, function($posicion) use ($pedidos, $c) {
+            $rutaRealizada = array_filter($posiciones, function($posicion) use ($pedidos, $c) {
                 $fechaPosicion = new DateTime($posicion["fecha"]);
                 $fechaInicio = new DateTime($pedidos[$c]["fechaInicio"]);
                 $fechaFin = new DateTime($pedidos[$c]["fechaFin"]);
             
                 return $fechaPosicion >= $fechaInicio && $fechaPosicion <= $fechaFin;
             });
+
+            $pedidos[$c]["rutaRealizada"] = [];
+            for( $x = 0; $x < count($rutaRealizada); $x++ ){
+                $pedidos[$c]["rutaRealizada"][] = ["lat" => $rutaRealizada["latitud"],"lng" => $rutaRealizada["longitud"]];
+            }
+
+            //$pedidos[$c]["rutaRealizada"];
         }
 
         echo '<script>';
