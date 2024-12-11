f177ffb7c9a6ec76edc119ada2baa6ff0dbc7a0c
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-12-11 15:26:06 -0700

    cambios

--- reproductor.php
+++ reproductor.php
@@ -185,7 +185,9 @@
 
             $pedidos[$c]["rutaRealizada"] = [];
             for($x = 0; $x < count($rutaRealizadaNor) - 1; $x++ ){
-
+                if($x != 0){
+                    break;
+                }
                 //if(\GeometryLibrary\SphericalUtil::computeDistanceBetween(['lat' => $rutaRealizadaNor[$x]["lat"], 'lng' => $rutaRealizadaNor[$x]["lng"]], ['lat' => $rutaRealizadaNor[$x+1]["lat"], 'lng' => $rutaRealizadaNor[$x+1]["lng"]]) >= 60 ){
                     //$curl = curl_init("http://10.10.10.130:8082/ors/v2/directions/driving-car?start=".$rutaRealizadaNor[$x]["lng"].",".$rutaRealizadaNor[$x]["lat"]."&end=".$rutaRealizadaNor[$x+1]["lng"].",".$rutaRealizadaNor[$x+1]["lat"]);
                     $curl = curl_init("http://10.10.10.130:8082/ors/v2/directions/driving-car?start=-108.97092714265861,25.798724605179547&end=-108.98301640736422,25.793071127573885");
