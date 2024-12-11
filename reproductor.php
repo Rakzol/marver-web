a79834cf52bd83b5a9ad8730cb56f944249f9ae9
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-12-11 15:14:54 -0700

    cambios

--- reproductor.php
+++ reproductor.php
@@ -187,7 +187,8 @@
             for($x = 0; $x < count($rutaRealizadaNor) - 1; $x++ ){
 
                 //if(\GeometryLibrary\SphericalUtil::computeDistanceBetween(['lat' => $rutaRealizadaNor[$x]["lat"], 'lng' => $rutaRealizadaNor[$x]["lng"]], ['lat' => $rutaRealizadaNor[$x+1]["lat"], 'lng' => $rutaRealizadaNor[$x+1]["lng"]]) >= 60 ){
-                    $curl = curl_init("http://10.10.10.130:8082/ors/v2/directions/driving-car?start=".$rutaRealizadaNor[$x]["lng"].",".$rutaRealizadaNor[$x]["lat"]."&end=".$rutaRealizadaNor[$x+1]["lng"].",".$rutaRealizadaNor[$x+1]["lat"]);
+                    //$curl = curl_init("http://10.10.10.130:8082/ors/v2/directions/driving-car?start=".$rutaRealizadaNor[$x]["lng"].",".$rutaRealizadaNor[$x]["lat"]."&end=".$rutaRealizadaNor[$x+1]["lng"].",".$rutaRealizadaNor[$x+1]["lat"]);
+                    $curl = curl_init("http://10.10.10.130:8082/ors/v2/directions/driving-car?start=-108.97092714265861,25.798724605179547&end=-108.98301640736422,25.793071127573885");
                     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     
                     $respuesta = curl_exec($curl);
@@ -197,7 +198,6 @@
                     $orsCoords = $respuestaJSON["features"][0]["geometry"]["coordinates"];
     
                     for($j = 0; $j < count($orsCoords); $j++ ){
-                        echo $j . ' - ';
                         $pedidos[$c]["rutaRealizada"][] = [ "lat" => $orsCoords[$j][1], "lng" => $orsCoords[$j][0] ];
                     }
                 //}
