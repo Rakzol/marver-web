8de218f442fd7f6d10b6510eaf1e3dc0661e4331
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2025-01-11 12:17:19 -0700

    cambios

---
+++ modelo/consultarCodigosPorPedido.php
@@ -0,0 +1,26 @@
+<?php
+    try{
+
+        switch( $_GET['sucursal'] ){
+            case 'mochis':
+                $conexion = new PDO('sqlsrv:Server=10.10.10.130;Database=Mochis;TrustServerCertificate=true','MARITE','2505M$RITE');
+            break;
+            case 'guasave':
+                $conexion = new PDO('sqlsrv:Server=12.12.12.254;Database=Guasave;TrustServerCertificate=true','MARITE','2505M$RITE');
+            break;
+            case 'higuera':
+                $conexion = new PDO('sqlsrv:Server=11.11.11.52;Database=Higuera;TrustServerCertificate=true','MARITE','2505M$RITE');
+                break;
+        }        
+
+        $conexion->setAttribute(PDO::SQLSRV_ATTR_FETCHES_NUMERIC_TYPE, True);
+
+        $preparada = $conexion->prepare("SELECT Codigo FROM PedidoDetalle WHERE Folio = :folio GROUP BY Codigo ORDER BY Codigo ASC");
+        $preparada->bindValue(":folio", $_GET["folio"]);
+        $preparada->execute();
+
+        echo json_encode($preparada->fetchAll(PDO::FETCH_ASSOC), JSON_UNESCAPED_UNICODE);
+    }catch( Exception $exception ) {
+        header('HTTP/1.1 500 ' . $exception->getMessage());
+    }
+?>
