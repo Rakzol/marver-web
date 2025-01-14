04341b850c72cb1cd41aeb44181afb476b067830
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2025-01-14 13:06:14 -0700

    cambios

--- api/marver.php
+++ api/marver.php
@@ -66,7 +66,7 @@
         else if($accion == "validar"){
             $token = obtenerJWT();
             $payload = validarJWT($token);
-            
+
             if($payload){
                 echo json_encode(["payload" => $payload], JSON_UNESCAPED_UNICODE);
                 exit();
@@ -75,9 +75,10 @@
             http_response_code(401);
             echo json_encode(["token" => $token, "error" => "Token invalido"], JSON_UNESCAPED_UNICODE);
         }
-
-        http_response_code(404);
-        echo json_encode(["error" => "Ruta no encontrada"], JSON_UNESCAPED_UNICODE);
+        else{
+            http_response_code(404);
+            echo json_encode(["error" => "Ruta no encontrada"], JSON_UNESCAPED_UNICODE);
+        }
         
     }catch( Exception $exception ) {
         http_response_code(500);
