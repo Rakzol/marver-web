bfb61f92e27da332ae2d8d8eeb918613bfd882ca
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2025-01-15 12:02:56 -0700

    cambios

--- api/api.php
+++ api/api.php
@@ -5,15 +5,15 @@
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-        http_response_code(200);
-        exit();
-    }
-
     try{
 
         require_once 'JWTHelper.php';
         
+        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+            http_response_code(200);
+            exit();
+        }
+
         $datos = json_decode(file_get_contents('php://input'), true);
         $sucursal = $datos['sucursal'] ?? null;
 
