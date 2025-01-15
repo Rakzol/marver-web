1b52c6dcc1d207ce6187cec30939e57ed3bfd011
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2025-01-15 11:46:19 -0700

    cambios

--- api/api.php
+++ api/api.php
@@ -1,7 +1,8 @@
 <?php
     header('Content-Type: application/json');
 
-    header("Access-Control-Allow-Origin: *"); // Cambia "*" a un dominio específico si es necesario
+    //header("Access-Control-Allow-Origin: *"); // Cambia "*" a un dominio específico si es necesario
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
