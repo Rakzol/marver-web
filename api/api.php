7048fcbf1c1ec40e612a489ba95de883b69380e1
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2025-01-15 11:41:12 -0700

    cambios

--- api/api.php
+++ api/api.php
@@ -1,6 +1,10 @@
 <?php
     header('Content-Type: application/json');
 
+    header("Access-Control-Allow-Origin: *"); // Cambia "*" a un dominio específico si es necesario
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
     try{
 
         require_once 'JWTHelper.php';
