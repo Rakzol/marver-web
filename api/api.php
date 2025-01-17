2bfcbb52d03f73e3ea1f7c0d4f4a95dbc8819af1
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2025-01-16 17:54:56 -0700

    cambios

--- api/api.php
+++ api/api.php
@@ -88,9 +88,8 @@
                     http_response_code(405);
                     echo json_encode(["error" => "Metodo no permitido"], JSON_UNESCAPED_UNICODE);
                 break;
-                
-                exit();
             }
+            exit();
         }else{
             $token = obtenerJWT();
             $payload = validarJWT($token);
