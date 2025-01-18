48dce7cbe62f5daadf291edce9b74f4cced5529a
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2025-01-18 09:34:07 -0700

    cambios

--- api/socket.php
+++ api/socket.php
@@ -1,120 +1,54 @@
 <?php
-// Dirección y puerto del servidor
+
 $host = '127.0.0.1';
-$port = 6969;
+$port = 8080;
 
-// Crear un socket TCP/IP
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
-socket_bind($socket, $host, $port);
-socket_listen($socket);
+$server = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_bind($server, $host, $port);
+socket_listen($server);
 
-echo "Servidor WebSocket iniciado en $host:$port\n";
-
-$clients = [$socket];
+$clients = [];
+echo "Servidor WebSocket iniciado en ws://$host:$port\n";
 
 while (true) {
+    // Monitorear nuevos clientes
     $read = $clients;
-    socket_select($read, $write = null, $except = null, 0, 10);
+    $read[] = $server;
+    socket_select($read, $write, $except, 0, 10);
 
-    // Detectar nuevas conexiones
-    if (in_array($socket, $read)) {
-        $newClient = socket_accept($socket);
+    if (in_array($server, $read)) {
+        $newClient = socket_accept($server);
         $clients[] = $newClient;
-
         echo "Nuevo cliente conectado.\n";
-        $key = array_search($socket, $read);
-        unset($read[$key]);
+        unset($read[array_search($server, $read)]);
     }
 
-    // Manejar mensajes de los clientes conectados
+    // Consultar la tabla de auditoría
+    $conn = new PDO("sqlsrv:Server=localhost;Database=your_database", "username", "password");
+    $stmt = $conn->query("SELECT * FROM products_audit WHERE action_sent = 0");
+    $changes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($changes as $change) {
+        $message = json_encode($change);
+        
+        // Enviar el cambio a todos los clientes conectados
+        foreach ($clients as $client) {
+            @socket_write($client, $message);
+        }
+
+        // Marcar el registro como enviado
+        $conn->query("UPDATE products_audit SET action_sent = 1 WHERE id = {$change['id']}");
+    }
+
+    // Leer mensajes de los clientes conectados
     foreach ($read as $client) {
-        $data = socket_read($client, 1024, PHP_BINARY_READ);
-        if (!$data) {
-            // Desconectar cliente
-            $index = array_search($client, $clients);
-            unset($clients[$index]);
+        $data = @socket_read($client, 1024, PHP_BINARY_READ);
+        if ($data === false || $data === "") {
+            unset($clients[array_search($client, $clients)]);
             socket_close($client);
             echo "Cliente desconectado.\n";
-            continue;
-        }
-
-        // Handshake inicial
-        if (strpos($data, 'GET') !== false) {
-            $headers = performHandshake($data, $client);
-            echo "Handshake completado con cliente.\n";
-            continue;
-        }
-
-        // Decodificar el mensaje recibido
-        $decodedData = decodeMessage($data);
-        echo "Mensaje recibido: $decodedData\n";
-
-        // Enviar respuesta
-        $response = encodeMessage("Servidor dice: $decodedData");
-        socket_write($client, $response);
-    }
-}
-
-socket_close($socket);
-
-// Realizar el handshake para establecer conexión WebSocket
-function performHandshake($data, $client) {
-    $lines = preg_split("/\r\n/", $data);
-    $headers = [];
-    foreach ($lines as $line) {
-        if (strpos($line, ': ') !== false) {
-            list($key, $value) = explode(': ', $line);
-            $headers[$key] = trim($value);
         }
     }
 
-    $secWebSocketKey = $headers['Sec-WebSocket-Key'];
-    $secWebSocketAccept = base64_encode(pack(
-        'H*',
-        sha1($secWebSocketKey . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')
-    ));
-
-    $upgradeHeaders = "HTTP/1.1 101 Switching Protocols\r\n" .
-        "Upgrade: websocket\r\n" .
-        "Connection: Upgrade\r\n" .
-        "Sec-WebSocket-Accept: $secWebSocketAccept\r\n\r\n";
-
-    socket_write($client, $upgradeHeaders);
-}
-
-// Decodificar mensaje recibido de WebSocket
-function decodeMessage($data) {
-    $payloadLen = ord($data[1]) & 127;
-    if ($payloadLen === 126) {
-        $masks = substr($data, 4, 4);
-        $payload = substr($data, 8);
-    } elseif ($payloadLen === 127) {
-        $masks = substr($data, 10, 4);
-        $payload = substr($data, 14);
-    } else {
-        $masks = substr($data, 2, 4);
-        $payload = substr($data, 6);
-    }
-
-    $text = '';
-    for ($i = 0; $i < strlen($payload); ++$i) {
-        $text .= $payload[$i] ^ $masks[$i % 4];
-    }
-
-    return $text;
-}
-
-// Codificar mensaje para enviar a WebSocket
-function encodeMessage($text) {
-    $b1 = 0x81; // 0x81 = FIN + texto
-    $length = strlen($text);
-
-    if ($length <= 125) {
-        return pack('CC', $b1, $length) . $text;
-    } elseif ($length <= 65535) {
-        return pack('CCn', $b1, 126, $length) . $text;
-    } else {
-        return pack('CCNN', $b1, 127, 0, $length) . $text;
-    }
+    sleep(1); // Intervalo para verificar nuevos cambios
 }
